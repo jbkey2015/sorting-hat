762e04eae6e4249a644fbfaf978f262710416bdc
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-09-17 00:10:10 -0500

    finally made it work somehow

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const printToDom = (stringToPrint, divId) => {
 
 const houses = ["Griffindor", "Hufflepuff", "Ravenclaw", "Slytheran"];
 
+const expel = [];
+
 const formMaker = () => {
     const formString = `
     <div class = "container card">
@@ -36,20 +38,40 @@ const cardMaker = () => {
         <div class="card-body">
             <p class="card-title">${cardInput}</p>
             <p class="card-text">${randomHouse}</p>
-            <a href="#" class="btn btn-primary">Expel</a>
+            <button type="button" class="btn btn-primary expel">Expel</button>
         </div>
         </div>
         </div>
         </div>
     </div>
     `;
-    printToDom(domString, "card");    
+    printToDom(domString, "card"); 
+    removeCard()
 }
+
 
 //document.getElementById("inputButton").addEventListener("click", cardMaker);
 
 document.getElementById("form").addEventListener("click", (event) => {
     if (event.target.id === "inputButton"){
         cardMaker()
-    }
+    } 
 })
+
+const removeCard = () => {
+    const cardsToRemove = document.getElementsByClassName('expel');
+    for(let i = 0; i < cardsToRemove.length; i++) {
+        const deleteCard = cardsToRemove[i];
+        deleteCard.addEventListener('click', (e) =>{
+            const btnClicked = e.target;
+            const cardsToRemove = btnClicked.parentNode.parentNode;
+            cardsToRemove.remove();
+        })
+    }
+};
+
+//expel Student Event Listener
+const expelStudent = () => {
+    $(".expel").on('click', removeCard);
+}
+expelStudent();
